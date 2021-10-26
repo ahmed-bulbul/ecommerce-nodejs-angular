7884f73ad885f7fa3aa2d4ba15965e6e7553b0da
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2021-10-26 07:57:36 +0600

    Admin Panel Start

--- Frontend/bluebits/apps/admin/src/app/app.module.ts
+++ Frontend/bluebits/apps/admin/src/app/app.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, SidebarComponent, ShellComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
